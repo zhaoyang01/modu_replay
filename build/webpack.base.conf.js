df1da4d81ac64f752162ecf0ddb2254bc5f3fe71
ZhaoY <1095263376@qq.com>
2019-12-31 09:53:14 +0800

    zhaoY

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -36,6 +36,8 @@ module.exports = {
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
+      'store': resolve(`src/store/index.js`),
+      'router': resolve(`src/router/index.js`)
     }
   },
   module: {
