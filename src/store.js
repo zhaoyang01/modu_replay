ca37df8583e7ee6e1f64b420840c2b671bdf60fe
ZhaoY <1095263376@qq.com>
2019-12-31 17:41:13 +0800

    zhaoY
    更新路由

---
+++ src/store.js
@@ -0,0 +1,42 @@
+import Vue from "vue";
+import Vuex from 'vuex';
+const home = () => import('@base/page/home/vue'); /* webpackChunkName: 'community-control' */ 
+Vue.use(Vuex);
+
+const state = {
+    // loading 状态 
+    loading: false,
+    // 弹出框状态
+    alert: false,
+    // 弹出框内容
+    alertValue: ''
+}
+
+const getters = {
+};
+
+const mutations = {
+    // 更新当前页面
+    updateLoading(state, flag){
+        state.start = index
+    },
+
+    // 更新数据总量
+    updateTotalCount(state, totel){
+        state.totalCount = totel;
+    },
+};
+
+const actions = {
+};
+
+const store = new Vuex.Store({
+    actions,
+    getters,
+    state,
+    mutations,
+    modules: {
+    }
+});
+
+export default store;
