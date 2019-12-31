2711ba5447ff4b9d82025ec8a8c98eaac5d41dd4
ZhaoY <1095263376@qq.com>
2019-12-31 09:58:24 +0800

    zhaoY
    修正route末模板

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+const HelloWorld = () => import('@base/pages/HelloWorld/vue'); /* webpackChunkName: 'community-control' */ 
 
 Vue.use(Router)
 
