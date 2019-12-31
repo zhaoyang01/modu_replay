63e5b664c739d304bfcdb68e9c98893304dac577
ZhaoY <1095263376@qq.com>
2019-12-31 15:38:00 +0800

    zhaoY
    增加全局常量

---
+++ src/assets/constants.js
@@ -0,0 +1,49 @@
+/**
+ * 常量
+ * @author zhaoY
+ * @version 1.0.0
+ */
+ 
+/**
+ * 输入长度限制
+ * @enum {Number}
+ * @readonly
+ */
+const MAX_LENGTH = {
+    INPUT: 30,
+    TEXTAREA: 200,
+    AUTHOR: 10
+};
+
+/**
+ * 年代标签
+ * @enum {Object}
+ * @readonly
+ */
+const LABEL_DATE = [
+    {name: '现代', id: 0},
+    {name: '1920年', id: 1},
+    {name: '架空时空', id: 2},
+];
+
+/**
+ * 区域标签
+ * @enum {Object}
+ * @readonly
+ */
+const LABEL_COUNTRY = [
+    {name: '欧洲', id: 0},
+    {name: '美国', id: 1},
+    {name: '日本', id: 2},
+    {name: '中国', id: 3},
+    {name: '架空', id: 4},
+]
+
+
+
+
+
+
+
+
+
